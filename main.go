a283e62113e49f5a4f5b2aa2dbb614abcb39c1e8
Ardit Zubaku <zubaku92@gmail.com>
2026-04-03 21:44:02 +0200

    scanning

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Lox struct {
+	hadError bool
+}
+
+func main() {
+	l := new(Lox)
+
+	if len(os.Args) > 2 {
+		fmt.Println("Usage: golox [script]")
+		os.Exit(64)
+	} else if len(os.Args) == 2 {
+		if err := l.runFile(os.Args[1]); err != nil {
+			// TODO: Rethink this
+			os.Exit(1)
+		}
+	} else {
+		l.runPrompt()
+	}
+}
+
+func (l *Lox) runFile(path string) error {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	l.run(bytes)
+
+	if l.hadError {
+		os.Exit(65)
+	}
+
+	return nil
+}
+
+func (l *Lox) run(code []byte) {
+	runes := []rune(string(code))
+
+	for _, token := range runes {
+		fmt.Println(string(token))
+	}
+}
+
+func (l *Lox) runPrompt() {
+	input := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		if ok := input.Scan(); !ok {
+			break
+		}
+
+		line := input.Bytes()
+		l.run(line)
+		l.hadError = false
+	}
+}
+
+func (l *Lox) err(line int, msg string) {
+	l.report(line, "", msg)
+}
+
+func (l *Lox) report(line int, where string, msg string) {
+	fmt.Printf("[line %d] Error %s: %s\n", line, where, msg)
+	l.hadError = true
+}
