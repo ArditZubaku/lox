716e3253dedd4dc9f84c43a5b6c0443d67c72025
Ardit Zubaku <zubaku92@gmail.com>
2026-04-03 21:43:22 +0200

    scanning: token type

---
+++ token_type.go
@@ -0,0 +1,53 @@
+package main
+
+type TokenType byte
+
+const (
+	// Single-character tokens.
+	LeftParen TokenType = iota // LeftParen
+	RightParen
+	LeftBrace
+	RightBrace
+	Comma
+	TokenTypeDot
+	Minus
+	Plus
+	Semicolon
+	Slash
+	Star
+
+	// One or two character tokens.
+	Bang
+	BangEqual
+	Equal
+	EqualEqual
+	Greater
+	GreaterEqual
+	Less
+	LessEqual
+
+	// Literals.
+	Identifier
+	String
+	Number
+
+	// Keywords.
+	And
+	Class
+	Else
+	False
+	Fun
+	For
+	If
+	Nil
+	Or
+	Print
+	Return
+	Super
+	This
+	True
+	Var
+	While
+
+	EOF
+)
