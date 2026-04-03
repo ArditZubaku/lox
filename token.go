116164db11517f5e1f601c21845892e4f431b359
Ardit Zubaku <zubaku92@gmail.com>
2026-04-03 21:43:46 +0200

    scanning: location information - token

---
+++ token.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Token struct {
+	Type    TokenType
+	Lexeme  string
+	Literal any
+	Line    int
+}
+
+func NewToken(
+	tokenType TokenType,
+	lexeme string,
+	literal any,
+	line int,
+) Token {
+	return Token{
+		Type:    tokenType,
+		Lexeme:  lexeme,
+		Literal: literal,
+		Line:    line,
+	}
+}
+
+func (t *Token) String() string {
+	return fmt.Sprintf("%s %s %v", string(t.Type), t.Lexeme, t.Literal)
+}
